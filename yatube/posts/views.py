6b55fd6f0368286d1c2cbe65115b82c33640879c
Dmitry Rotanin <rotanin@gmail.com>
2022-06-15 17:49:12 +0300

    create project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts':posts,
+        'title':title,
+    }
+    return render(request, template, context) 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Пост {slug} должен быть тут')
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    title = f'Записи сообщества {group.title}'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title':title,
+    }
+    return render(request, template, context)
+
